d441d91b0ed816f331ab72355bfffd4a7c5b9158
Fabio Rossato <fabio.rossato@we-race.it>
2026-04-07 10:54:50 +0200

    Update firmware version to 1.2.2-beta

--- Halo-F1.ino
+++ Halo-F1.ino
@@ -26,9 +26,9 @@ const int DRIVERS_NUMBER = 22;
 #define SCREEN_HEIGHT 480
 
 #ifdef TOUCH_CAPACITIVE
-const String fw_version = "1.2.1";
+const String fw_version = "1.2.2-beta";
 #else
-const String fw_version = "1.2.1-R";
+const String fw_version = "1.2.2-R-beta";
 #endif
 
 
